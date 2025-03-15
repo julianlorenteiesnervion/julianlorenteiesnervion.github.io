a152d0a30669b02c6ff5b7e17c01dd065eb5ff67
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-03-15 23:58:02 +0100

    Página actualizada.

--- pruebaX/view.js
+++ pruebaX/view.js
@@ -5,16 +5,19 @@ function mostrarPost(Post) {
     let likes = parseInt(Math.random() * 10000);
     let views = parseInt(Math.random() * 100);
 
+    let randomImageUrl = "https://picsum.photos/200?" + new Date().getTime();
+    let randomMediaUrl = "https://picsum.photos/700/300?" + new Date().getTime();
+
     posts.innerHTML += `<div class="post">
         <div class="post_user">
-            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" class="profile_picture">
+            <img src="` + randomImageUrl + `" class="profile_picture">
             <h3 class="user_name">` + Post.getUser() + `</h3>
             <h3 class="user_id">@` + Post.getUser() + `</h3>
-            <h3 class="post_time">Hace ` + Post.getTime() + ` horas</h3>
+            <h3 class="post_time">· Hace ` + Post.getTime() + ` horas</h3>
         </div>
         <div class="post_info">
             <p class="post_text">` + Post.getMessage() + `</p>
-            <img src="https://t3.ftcdn.net/jpg/09/03/91/64/360_F_903916468_L5Oli1cL45LG0I58JTVPbNv23khMK5y9.jpg" class="post_media">
+            <img src="` + randomMediaUrl + `" class="post_media">
         </div>
         <div class="post_bar">
             <h4 id="comments">🗨️ ` + comments + `</h4>
