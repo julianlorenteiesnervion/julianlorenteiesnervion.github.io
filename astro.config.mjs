c2cbb60f550582f120744917d69ade73d58210ae
julianlorenteiesnervion <julian.lorente@iesnervion.es>
2025-12-16 18:17:30 +0100

    Website uploaded!

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
-    }
+    },
+    site: "https://julianlorenteiesnervion.github.io"
 });
